9edc1747a31bdb7618bbc068bc419ecf16d2ff70
giulia lombardi <giulia.lombardi@fermi.mo.it>
2025-11-21 11:26:07 +0100

    file per settare il db

---
+++ set_db.py
@@ -0,0 +1,262 @@
+import asyncio
+from pymongo import AsyncMongoClient
+
+
+async def main():
+    client = AsyncMongoClient('localhost', 27017)
+    db = client["publisher_db"]
+    publishers= db["publishers"]
+    books=db["books"]
+    await aggiungi_publishers(publishers, books)
+
+async def aggiungi_publishers(pubs, books):
+    await pubs.insert_many([
+    {
+    "name": "Einaudi",
+    "founded_year": 1933,
+    "country": "Italia"
+    },
+    {
+    "name": "Penguin Random House",
+    "founded_year": 2013,
+    "country": "USA"
+    },
+    {
+    "name": "Mondadori",
+    "founded_year": 1907,
+    "country": "Italia"
+    },
+    {
+    "name": "HarperCollins",
+    "founded_year": 1989,
+    "country": "USA"
+    },
+    {
+    "name": "Feltrinelli",
+    "founded_year": 1954,
+    "country": "Italia"
+    }
+    ])
+
+    books_list=[
+        {
+            "title": "Il barone rampante",
+            "author": "Italo Calvino",
+            "genre": "Romanzo",
+            "year": 1957,
+            "publisher_id": "OBJECT_ID_EINAUDI"
+        },
+        {
+            "title": "Se una notte d'inverno un viaggiatore",
+            "author": "Italo Calvino",
+            "genre": "Romanzo",
+            "year": 1979,
+            "publisher_id": "OBJECT_ID_EINAUDI"
+        },
+        {
+            "title": "Il nome della rosa",
+            "author": "Umberto Eco",
+            "genre": "Giallo",
+            "year": 1980,
+            "publisher_id": "OBJECT_ID_EINAUDI"
+        },
+        {
+            "title": "Il codice da Vinci",
+            "author": "Dan Brown",
+            "genre": "Giallo",
+            "year": 2003,
+            "publisher_id": "OBJECT_ID_PENGUIN"
+        },
+        {
+            "title": "Harry Potter e la pietra filosofale",
+            "author": "J.K. Rowling",
+            "genre": "Fantasy",
+            "year": 1997,
+            "publisher_id": "OBJECT_ID_PENGUIN"
+        },
+        {
+            "title": "Il signore degli anelli",
+            "author": "J.R.R. Tolkien",
+            "genre": "Fantasy",
+            "year": 1954,
+            "publisher_id": "OBJECT_ID_PENGUIN"
+        },
+        {
+            "title": "1984",
+            "author": "George Orwell",
+            "genre": "Romanzo",
+            "year": 1949,
+            "publisher_id": "OBJECT_ID_MONDADORI"
+        },
+        {
+            "title": "Hunger Games",
+            "author": "Suzanne Collins",
+            "genre": "Fantasy",
+            "year": 2008,
+            "publisher_id": "OBJECT_ID_MONDADORI"
+        },
+        {
+            "title": "La ragazza del treno",
+            "author": "Paula Hawkins",
+            "genre": "Giallo",
+            "year": 2015,
+            "publisher_id": "OBJECT_ID_MONDADORI"
+        },
+        {
+            "title": "Harry Potter e il prigioniero di Azkaban",
+            "author": "J.K. Rowling",
+            "genre": "Fantasy",
+            "year": 1999,
+            "publisher_id": "OBJECT_ID_HARPERCOLLINS"
+        },
+        {
+            "title": "Il piccolo principe",
+            "author": "Antoine de Saint-Exupéry",
+            "genre": "Romanzo",
+            "year": 1943,
+            "publisher_id": "OBJECT_ID_HARPERCOLLINS"
+        },
+        {
+            "title": "Il vecchio e il mare",
+            "author": "Ernest Hemingway",
+            "genre": "Romanzo",
+            "year": 1952,
+            "publisher_id": "OBJECT_ID_HARPERCOLLINS"
+        },
+        {
+            "title": "Sostiene Pereira",
+            "author": "Antonio Tabucchi",
+            "genre": "Romanzo",
+            "year": 1994,
+            "publisher_id": "OBJECT_ID_FELTRINELLI"
+        },
+        {
+            "title": "La ragazza del treno",
+            "author": "Paula Hawkins",
+            "genre": "Giallo",
+            "year": 2015,
+            "publisher_id": "OBJECT_ID_FELTRINELLI"
+        },
+        {
+            "title": "Cecità",
+            "author": "José Saramago",
+            "genre": "Romanzo",
+            "year": 1995,
+            "publisher_id": "OBJECT_ID_FELTRINELLI"
+        }
+    ]
+    publishers=pubs.find()
+    pub_names_ids={}
+    async for pub in publishers:
+        pub_names_ids[pub["name"]]=str(pub["_id"])
+    #print(pub_names_ids)
+    await books.insert_many([
+  {
+    "title": "Il barone rampante",
+    "author": "Italo Calvino",
+    "genre": "Romanzo",
+    "year": 1957,
+    "publisher_id": pub_names_ids["Einaudi"]
+  },
+  {
+    "title": "Se una notte d'inverno un viaggiatore",
+    "author": "Italo Calvino",
+    "genre": "Romanzo",
+    "year": 1979,
+    "publisher_id": pub_names_ids["Einaudi"]
+  },
+  {
+    "title": "Il nome della rosa",
+    "author": "Umberto Eco",
+    "genre": "Giallo",
+    "year": 1980,
+    "publisher_id": pub_names_ids["Einaudi"]
+  },
+  {
+    "title": "Il codice da Vinci",
+    "author": "Dan Brown",
+    "genre": "Giallo",
+    "year": 2003,
+    "publisher_id": pub_names_ids["Penguin Random House"]
+  },
+  {
+    "title": "Harry Potter e la pietra filosofale",
+    "author": "J.K. Rowling",
+    "genre": "Fantasy",
+    "year": 1997,
+    "publisher_id": pub_names_ids["Penguin Random House"]
+  },
+  {
+    "title": "Il signore degli anelli",
+    "author": "J.R.R. Tolkien",
+    "genre": "Fantasy",
+    "year": 1954,
+    "publisher_id": pub_names_ids["Penguin Random House"]
+  },
+  {
+    "title": "1984",
+    "author": "George Orwell",
+    "genre": "Romanzo",
+    "year": 1949,
+    "publisher_id": pub_names_ids["Mondadori"]
+  },
+  {
+    "title": "Hunger Games",
+    "author": "Suzanne Collins",
+    "genre": "Fantasy",
+    "year": 2008,
+    "publisher_id": pub_names_ids["Mondadori"]
+  },
+  {
+    "title": "La ragazza del treno",
+    "author": "Paula Hawkins",
+    "genre": "Giallo",
+    "year": 2015,
+    "publisher_id": pub_names_ids["Mondadori"]
+  },
+  {
+    "title": "Harry Potter e il prigioniero di Azkaban",
+    "author": "J.K. Rowling",
+    "genre": "Fantasy",
+    "year": 1999,
+    "publisher_id": pub_names_ids["HarperCollins"]
+  },
+  {
+    "title": "Il piccolo principe",
+    "author": "Antoine de Saint-Exupéry",
+    "genre": "Romanzo",
+    "year": 1943,
+    "publisher_id": pub_names_ids["HarperCollins"]
+  },
+  {
+    "title": "Il vecchio e il mare",
+    "author": "Ernest Hemingway",
+    "genre": "Romanzo",
+    "year": 1952,
+    "publisher_id": pub_names_ids["HarperCollins"]
+  },
+  {
+    "title": "Sostiene Pereira",
+    "author": "Antonio Tabucchi",
+    "genre": "Romanzo",
+    "year": 1994,
+    "publisher_id": pub_names_ids["Feltrinelli"]
+  },
+  {
+    "title": "La ragazza del treno",
+    "author": "Paula Hawkins",
+    "genre": "Giallo",
+    "year": 2015,
+    "publisher_id": pub_names_ids["Feltrinelli"]
+  },
+  {
+    "title": "Cecità",
+    "author": "José Saramago",
+    "genre": "Romanzo",
+    "year": 1995,
+    "publisher_id": pub_names_ids["Feltrinelli"]
+  }
+])
+
+if __name__ == "__main__":
+    asyncio.run(main())
